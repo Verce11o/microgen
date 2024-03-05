5ce43348360bf1845f75a198936a6c7a6c71aeb6
Verce11o <taymas_rashitov@mail.ru>
2024-03-05 22:03:20 +0500

    example service

--- example/internal/app/app.go
+++ example/internal/app/app.go
@@ -1,1 +1,32 @@
 package app
+
+import (
+	"context"
+	"fmt"
+	"github.com/Verce11o/example/internal/config"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	cfg        *config.Config
+	log        *zap.SugaredLogger
+	grpcServer *grpc.Server
+}
+
+func New(ctx context.Context, cfg *config.Config, log *zap.SugaredLogger) *App {
+
+	repo := repository.NewRepository(db, cfg)
+
+	services := service.NewService(log, repo)
+
+	server := grpc.NewServer()
+
+	example.Register(log, services, server, trace.Tracer)
+
+	return &App{
+		cfg:        cfg,
+		log:        log,
+		grpcServer: server,
+	}
+}
